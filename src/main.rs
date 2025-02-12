2af76c3b5fd06ecf48e16ef9b80b6495fa6d6fba
stinta <kyrbynyt@gmail.com>
2025-02-12 15:47:54 +0200

    yippie now we are using nom :)

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use thiserror::Error;
 fn main() -> AppResult {
     let config = Config::parse();
     if let Err(e) = config.validate() {
-        eprintln!("Error: {}", e);
+        eprintln!("Error: {e}");
         eprintln!("Run with -h to print help");
         std::process::exit(1);
     }
@@ -16,27 +16,27 @@ fn main() -> AppResult {
     let input_content =
         std::fs::read_to_string(&config.input_name).change_context(AppError::FailedToReadInput)?;
 
-    let targets = target::Makefile::from_str(input_content.as_str())
-        .change_context(AppError::FailedToParseBuildFile)
+    let targets = target::parse_makefile(&input_content)
+        .ok_or(AppError::FailedToParseBuildFile)
         .attach_printable("failed to parse the .msb file")?;
 
     if config.print_targets {
         for (i, target) in targets.get_targets().iter().enumerate() {
             println!("{}: {}", i, target.name());
             if target.file_dependencies().is_empty() && target.target_dependencies().is_empty() {
-                println!("Does not depend on anything")
+                println!("Does not depend on anything");
             } else {
                 println!("Depends on:");
                 if !target.file_dependencies().is_empty() {
                     println!("  These files:");
                     for f in target.file_dependencies() {
-                        println!("    {}", f);
+                        println!("    {f}");
                     }
                 }
                 if !target.target_dependencies().is_empty() {
                     println!("  These targets:");
                     for t in target.target_dependencies() {
-                        println!("    {}", t);
+                        println!("    {t}");
                     }
                 }
             }
